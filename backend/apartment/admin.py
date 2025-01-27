8d2dda1c4abd683b723aa10fe43610ecbd8cdec6
bishwajit93 <bish.karm123@gmail.com>
2025-01-28 00:01:12 +0100

    admin panel update

--- backend/apartment/admin.py
+++ backend/apartment/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Apartment, Owner
+from .models import Apartment, Owner, Room, Tenant
 # Register your models here.
 admin.site.register(Apartment)
 admin.site.register(Owner)
+admin.site.register(Room)
+admin.site.register(Tenant)
